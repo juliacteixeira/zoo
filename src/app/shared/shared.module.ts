72ecc7555af8abeb36866a4d5614ba15a6d3a061
Julia Teixeira <juuhc.teixeira@gmail.com>
2023-05-21 20:54:49 -0300

    header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
