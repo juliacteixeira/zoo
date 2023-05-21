b414faf18e6579c56915f79031698cf9ff8689bb
Julia Teixeira <juuhc.teixeira@gmail.com>
2023-05-21 20:54:49 -0300

    header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from '@shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModulesModule } from './modules/modules.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ModulesModule],
+  imports: [BrowserModule, AppRoutingModule, ModulesModule, SharedModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
